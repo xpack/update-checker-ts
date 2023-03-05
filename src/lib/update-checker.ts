829649159a5978401e84977c6972583078f4274f
Liviu Ionescu <ilg@livius.net>
2023-03-05 15:12:51 +0200

    add more typedoc metadata

--- src/lib/update-checker.ts
+++ src/lib/update-checker.ts
@@ -11,6 +11,11 @@
 
 // ----------------------------------------------------------------------------
 
+/**
+ * This file implements a class to check if a new version of a
+ * package is available, and notify the user via the log.
+ */
+
 // ----------------------------------------------------------------------------
 
 // https://nodejs.org/docs/latest-v14.x/api/
@@ -65,11 +70,37 @@ const defaultCheckUpdatesIntervalSeconds: number = 24 * 60 * 60
 
 // ============================================================================
 
+/**
+ * Type of the object defining the parameters to be passed to the
+ * **UpdateChecker** constructor.
+ */
 export interface UpdateCheckerConstructorParameters {
+  /** A reference to a Logger instance. */
   log: Logger
+  /**
+   * A string with the full package name, as from the `name`
+   * property in the `package.json` file.
+   */
   packageName: string
+  /**
+   * A string with the package version, in semver, as from the
+   * `version` property in the `package.json` file.
+   */
   packageVersion: string
+  /**
+   * A string with the path to the folder where the timestamps are located.
+   *
+   * If missing, by default, timestamps are created in
+   * the `$HOME/.config/timestamps` folder.
+   */
   timestampsFolderPath?: string
+  /**
+   * A number with the minimum interval between checks,
+   * in milliseconds,
+   *
+   * If missing, by default, checks are performed no sooner than
+   * 24 hours.
+   */
   checkUpdatesIntervalSeconds?: number
 }
 
@@ -86,30 +117,109 @@ export interface UpdateCheckerTestEnvironment {
   isInstalledAsRoot?: boolean
 }
 
+/**
+ * @summary The **UpdateChecker** class can be used to check and notify if
+ *   there is a newer version of a **npm** package.
+ *
+ * @description
+ * Checking the version requires an access to the packages registry,
+ * which might take some time to complete.
+ *
+ * Given that the result is needed later, before exiting,
+ * the operation can be started asynchronously and awaited at the end.
+ */
 export class UpdateChecker {
   // --------------------------------------------------------------------------
 
+  /**
+   * @summary The latest version of the package.
+   *
+   * @description
+   * This variable holds the result of calling the
+   * [`latest-version`](https://www.npmjs.com/package/latest-version) module.
+   *
+   * The content is a string in semver format.
+   */
   latestVersion: string | undefined = undefined
+  /**
+   * @summary The absolute path to the timestamp file.
+   *
+   * @description
+   * To avoid repeating the check too often, the timestamp of the
+   * latest check is remembered as the creation file of a file, in
+   * the `$HOME/.config/timestamps` folder.
+   *
+   * The content is a string with the absolute path of the file created
+   * after checking the version.
+   */
   timestampFilePath: string
+  /**
+   * @summary The possible error returned while retrieving the version.
+   *
+   * @description
+   * In case an error occurs while retrieving the version, the error
+   * message is logged, but the `Error` object is also stored in this
+   * variable, accessible to the caller.
+   */
   returnedError: any | undefined = undefined
 
+  /** A reference to a Logger instance. */
   protected log: Logger
+  /** A string with the full package name, as from the `package.json`. */
   protected packageName: string
+  /** A string with the package version, in semver, as from the
+   * `package.json`. */
   protected packageVersion: string
+  /** A string with the path to the folder where the stamps are created. */
   protected timestampsFolderPath: string
+  /** A number with the minimum interval to check for the updates,
+   * in milliseconds */
   protected checkUpdatesIntervalMilliseconds: number
+  /** A promise which resolves to a string with the latest version. */
   protected latestVersionPromise: Promise<string> | undefined = undefined
 
+  /** A map with environment variables. */
   protected processEnv: any
+  /** A boolean True when running in a CI environment. */
   protected isCI: boolean = false
+  /** A boolean True when running in a terminal. */
   protected isTTY: boolean = false
+  /** A boolean True when running with administrative credentials. */
   protected isRunningAsRoot: boolean = false
+  /** A boolean True when the package is installed globally. */
   protected isInstalledGlobally: boolean = false
+  /** A boolean True when the package is installed with
+   * administrative rights. */
   protected isInstalledAsRoot: boolean = false
 
+  // --------------------------------------------------------------------------
+  // Static properties.
+
+  /**
+   * @summary Mock environment to be used during tests.
+   *
+   * @description
+   * This property is normally undefined, but it can be
+   * set to a mock environment, during tests.
+   */
   static testEnvironment: UpdateCheckerTestEnvironment | undefined
 
-  // Constructor: use parent definition.
+  // --------------------------------------------------------------------------
+  // Constructor.
+
+  /**
+   * @summary Create an **UpdateChecker** instance.
+   *
+   * @param params The generic object used to pass parameters to the
+   * constructor.
+   *
+   * @description
+   * This function initialises the internal variables with lots of
+   * conditions tested later to determine if the update must be performed.
+   *
+   * To improve testability, a mock environment can be passed via a
+   * a static property.
+   */
   constructor (params: UpdateCheckerConstructorParameters) {
     assert(params)
 
@@ -195,16 +305,23 @@ export class UpdateChecker {
   }
 
   /**
-   * @summary Initiate a procedure to retrieve the version.
+   * @summary Initiate the asynchronous procedure to retrieve the version.
    *
    * @returns Nothing.
    *
    * @description
-   * Since this is a potentially long operation, it is initiated at the
-   * beginning and executed at the end, giving it a chance to run in
-   * parallel.
+   * The operation is performed by the third party package
+   * [`latest-version`](https://www.npmjs.com/package/latest-version),
+   * which accesses the location where the package is stored
+   * (like the `npmjs.com` server, or GitHub).
    *
-   * The operation is not started when running in a CI environment,
+   * Since this is a potentially long operation, it is expected to be
+   * started at the
+   * beginning of a CLI application and completed at the end, giving
+   * it a good chance to run in parallel.
+   *
+   * The operation is **not** started if it is considered
+   * not useful, like when running in a CI environment,
    * when not running from a console, or the environment variable
    * `NO_NPM_UPDATE_NOTIFIER` is defined.
    */
@@ -234,13 +351,13 @@ export class UpdateChecker {
   }
 
   /**
-   * @summary Compare versions and notify if an update is available.
+   * @summary Compare the versions and notify if an update is available.
    *
    * @returns Nothing.
    *
    * @description
-   * Await for the actual version to be retrieved, compare with the
-   * current version, possibly notify and write a new timestamp.
+   * Await for the actual version to be retrieved, compare it with the
+   * current version, possibly notify and create a new timestamp.
    */
   async notifyIfUpdateIsAvailable (): Promise<void> {
     const log = this.log
@@ -261,7 +378,7 @@ export class UpdateChecker {
       log.trace(`${this.constructor.name}:` +
       ` latest ${this.packageName}${latestVersion}`)
 
-      // Prevent further calls.
+      // Clear the variable to prevent further calls.
       this.latestVersionPromise = undefined
 
       this.latestVersion = latestVersion
@@ -272,10 +389,10 @@ export class UpdateChecker {
       }
 
       if (this.isRunningAsRoot) {
-      // When running as root, skip writing the timestamp to avoid
-      // later EACCES or EPERM. The effect is that the check will
-      // be performed with each run.
-      /* istanbul ignore next */
+        // When running as root, skip writing the timestamp to avoid
+        // later EACCES or EPERM. The effect is that the check will
+        // be performed with each run.
+        /* istanbul ignore next */
         if (os.platform() !== 'win32') {
           log.trace(`${this.constructor.name}:` +
           ` geteuid() ${process.geteuid()} != ${process.getuid()}`)
@@ -291,6 +408,7 @@ export class UpdateChecker {
       } else {
         log.warn(err.message)
       }
+      // Also return the error to the caller.
       this.returnedError = err
     }
 
@@ -309,14 +427,23 @@ export class UpdateChecker {
   /**
    * @summary Send the notification message.
    *
-   * @param latestVersion A string with the latest version,
-   *  in semver format.
    * @return Nothing.
    *
    * @description
-   * The notification is sent by writing to the log.
+   * The notification message is composed using the values stored
+   * in the instance properties.
+   *
+   * The message is sent by writing to the log.
    * This can be silenced by changing the log level.
-   * To customise the behaviour, this function can be redefined
+   *
+   * The notification message looks like this:
+   *
+   * ```text
+   * >>> New version 0.14.9 -> 0.15.0 available. <<<
+   * >>> Run 'npm install --global xpm@0.15.0' to update. <<<
+   * ```
+   *
+   * To customise this message, this function can be redefined
    * in a class derived from this one.
    *
    * @override
@@ -346,6 +473,10 @@ export class UpdateChecker {
    * @param ageMilliseconds Age in milliseconds.
    * @returns True if there is no timestamp or if it is older
    *  than the given age.
+   *
+   * @description
+   * Compute the timestamp age (as the time difference between the current
+   * time and the timestamp) and compare to the given age.
    */
   protected async didIntervalExpire (
     ageMilliseconds: number
@@ -370,6 +501,11 @@ export class UpdateChecker {
     return true
   }
 
+  /**
+   * @summary Read the stats of the timestamp file.
+   *
+   * @returns A promise that resolves to a fs.Stats object or null.
+   */
   protected async readTimestampFileStats (): Promise<fs.Stats | null> {
     const log = this.log
 
@@ -386,12 +522,12 @@ export class UpdateChecker {
   }
 
   /**
-   * @summary Remove the file used as timestamp
+   * @summary Remove the file used as timestamp.
    *
    * @returns Nothing.
    *
    * @description
-   * The main reason this is a separate function is testability.
+   * The main reason for this to be a separate function is testability.
    */
   protected async clearTimestamp (): Promise<void> {
     const log = this.log
@@ -400,6 +536,14 @@ export class UpdateChecker {
     await deleteAsync(this.timestampFilePath, { force: true })
   }
 
+  /**
+   * @summary Create the file used as timestamp.
+   *
+   * @returns Nothing.
+   *
+   * @description
+   * Create an empty file. The timestamp is the creation date.
+   */
   protected async createTimestamp (): Promise<void> {
     const log = this.log
     log.trace(`${this.constructor.name}.createTimestamp()`)
